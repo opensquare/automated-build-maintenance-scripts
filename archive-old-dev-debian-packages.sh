b1269823610c943f94c321014fc224d9d2128e6e
Kai Kewley <kaikewley@gmail.com>
2013-06-25 13:32:27 +0100

    Debian and Redhat package archive scripts for cron
    These scripts archive all but the latest two versions of each
    package in order to keep repository re-indexing, and the builds,
    running quickly.

---
+++ archive-old-dev-debian-packages.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+latest_packages_to_keep=2
+archive_dir='.old'
+
+# Stop on error
+set -e
+
+function archive {
+        mkdir -p $archive_dir
+        for p in `ls *.deb | sort | sed 's/_.*//' | uniq`
+        do
+                echo $p
+                old_files="`ls ${p}_* | head -n-$latest_packages_to_keep`"
+                if [[ `echo $old_files | wc -w` > 0 ]]
+                then
+                        mv $old_files $archive_dir
+                fi
+        done
+}
+
+cd dists/all/main/binary-amd64
+archive
+cd -
+
+cd dists/all/main/binary-i386
+archive
+cd -
---
+++ archive-old-dev-redhat-packages.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+latest_packages_to_keep=2
+archive_dir='.old'
+
+# Stop on error
+set -e
+
+function archive {
+        mkdir -p $archive_dir
+        for p in `ls *.rpm | sed 's/[0-9]*\.[0-9]*\-[0-9]*\.noarch\.rpm//' | sort | uniq`
+        do
+                echo $p
+                old_files="`ls ${p}* | head -n-$latest_packages_to_keep`"
+                if [[ `echo $old_files | wc -w` > 0 ]]
+                then
+                        mv $old_files $archive_dir
+                fi
+        done
+}
+
+archive
+
